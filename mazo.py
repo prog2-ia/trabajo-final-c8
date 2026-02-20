5b363f0aad45381a50177e0573d9394f0b4416d9
sofiaaperezz <spv34@alu.ua.es>
2026-02-20 13:19:33 +0100

    Estructura clases Tarjeta y Mazo

--- mazo.py
+++ mazo.py
@@ -1,2 +1,14 @@
 class Mazo:
+    def __init__(self, nombre):
+    def anadir_tarjeta(self):
+
+    def eliminar_tarjeta(self):
+
+    def mezclar_tarjetas(self):
+
+    def unir_mazos(self):
+
+    def comparar_mazos(self):
+
+    def contar_tarjetas(self):
     pass
--- tarjeta.py
+++ tarjeta.py
@@ -1,2 +1,10 @@
 class Tarjeta
+    def __init__(self, palabra, traduccion, categoria, nivel_dificultad):
+
+    def guardar_info(self):
+
+    def es_correcta(self):
+
+    def duplicados(self):
+
     pass
