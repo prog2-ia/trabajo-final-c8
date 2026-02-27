81ff7c14156204a3e74abbc81b8320c99fae73bd
sofiaaperezz <spv34@alu.ua.es>
2026-02-27 14:33:28 +0100

    clase Mazo acabada

--- mazo.py
+++ mazo.py
@@ -1,4 +1,5 @@
 from tarjeta import Tarjeta
+import random
 
 class Mazo:
     '''
@@ -6,15 +7,67 @@ class Mazo:
     '''
     def __init__(self, nombre):
         self.nombre=nombre
-    def anadir_tarjeta(self):
+        self._tarjetas=[]
 
-    def eliminar_tarjeta(self):
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, valor):
+        if not valor or valor.strip()=="":
+            raise ValueError("El nombre no puede estar vacío")
+        self._nombre=valor.strip()
+
+    @property
+    def tarjeta(self):
+        return self._tarjetas.copy()
+
+    def anadir_tarjeta(self, tarjeta):
+        if not isinstance(tarjeta, Tarjeta):
+            raise TypeError("El objeto debe ser de tipo Tarjeta")
+        if tarjeta in self._tarjetas:
+            raise ValueError("La tarjeta ya existe en el mazo")
+        self._tarjetas.append(tarjeta)
+
+    def eliminar_tarjeta(self, tarjeta):
+        if tarjeta not in self._tarjetas:
+            raise ValueError("La tarjeta no está en el mazo")
+        self._tarjetas.remove(tarjeta)
 
     def mezclar_tarjetas(self):
+        random.shuffle(self._tarjetas)
 
-    def unir_mazos(self):
+    def __add__(self, other):
+        if not isinstance(other, Mazo):
+            raise TypeError("Solo se pueden unir objetos de tipo Mazo")
 
-    def comparar_mazos(self):
+        nuevo_mazo=Mazo(f"{self.nombre} + {other.nombre}")
 
-    def contar_tarjetas(self):
-    pass
+        for tarjeta in self._tarjetas:
+            nuevo_mazo.añadir_tarjeta(tarjeta)
+
+        for tarjeta in other._tarjetas:
+            if tarjeta not in nuevo_mazo._tarjetas:
+                nuevo_mazo.añadir_tarjeta(tarjeta)
+        return nuevo_mazo
+
+    def __eq__(self, other):
+        if not isinstance(other, Mazo):
+            return False
+
+        if len(self._tarjetas) != len(other._tarjetas):
+            return False
+
+        for tarjeta in self._tarjetas:
+            if tarjeta not in other._tarjetas:
+                return False
+        return True
+
+    def __len__(self):
+        return len(self._tarjetas)
+
+    def __str__(self):
+        return (f"Mazo: {self.nombre}\n"
+                f"Número de tarjetas: {len(self)}")
+
--- tarjeta.py
+++ tarjeta.py
@@ -1,9 +1,9 @@
 class Tarjeta:
-    ''''
+    """
     Representa una palabra individual con su traducción, categoría y nivel de dificultad. 
     Permite comprobar si una respuesta es correcta y comparar tarjetas para evitar duplicados. 
     Es la unidad básica del sistema.
-    ''''
+    """
     def __init__(self, palabra, traduccion, categoria, nivel_dificultad):
         self.palabra=palabra
         self.traduccion=traduccion
