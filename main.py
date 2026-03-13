ae3fbaa487e7aa019b6b84f98e24e68ecc16a3fc
Lucia González Mandler <lgm220@alu.ua.es>
2026-03-13 13:19:39 +0100

    importaciones creadas

---
+++ main.py
@@ -0,0 +1,14 @@
+# Importaciones necesarias
+
+from modelos.tarjeta import Tarjeta
+from modelos.mazo import Mazo
+from modelos.usuario import Usuario
+
+from ejercicios.ejercicio import Ejercicio
+from ejercicios.ejercicio_escritura import EjercicioEscritura
+from ejercicios.ejercicio_test import EjercicioTest
+
+from gestion.generador_ejercicios import GeneradorEjercicios
+from gestion.gestor_mazos import GestorMazos
+
+from datos.persistencia import Persistencia
