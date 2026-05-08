abb1e8387d4cc2f9291aee2a49ed51c96982ab5d
sofiaaperezz <spv34@alu.ua.es>
2026-05-08 12:11:16 +0200

    Update persistencia.py

--- datos/persistencia.py
+++ datos/persistencia.py
@@ -1,2 +1,24 @@
-class Persistencia():
-    pass
+import pickle
+import os
+
+class Persistencia:
+        RUTA = "datos.pkl"
+
+        @staticmethod
+        def guardar(usuario, mazo):
+            """
+            Guarda el usuario y el mazo en un archivo.
+            """
+            with open(Persistencia.RUTA, "wb") as f:
+                pickle.dump((usuario, mazo), f)
+
+        @staticmethod
+        def cargar():
+            """
+            Carga el usuario y el mazo si existen.
+            """
+            if not os.path.exists(Persistencia.RUTA):
+                return None, None
+
+            with open(Persistencia.RUTA, "rb") as f:
+                return pickle.load(f)
