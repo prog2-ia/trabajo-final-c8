2db0b5ce91fbdc9972322e1d288cff919d75dde9
sofiaaperezz <spv34@alu.ua.es>
2026-05-08 13:10:30 +0200

    clase gestor_mazos hecha

--- gestion/gestor_mazos.py
+++ gestion/gestor_mazos.py
@@ -1,2 +1,33 @@
-class GestorMazos():
-    pass
+from modelos.mazo import Mazo
+
+class GestorMazos:
+    """
+    Clase encargada de gestionar uno o varios mazos.
+    """
+
+    def __init__(self):
+        self._mazos = []
+
+    def crear_mazo(self, nombre):
+        mazo = Mazo(nombre)
+        self._mazos.append(mazo)
+        return mazo
+
+    def anadir_mazo(self, mazo):
+        if not isinstance(mazo, Mazo):
+            raise TypeError("Debe ser un objeto Mazo")
+        self._mazos.append(mazo)
+
+    def obtener_mazos(self):
+        return self._mazos
+
+    def obtener_mazo(self, nombre):
+        for mazo in self._mazos:
+            if mazo.nombre == nombre:
+                return mazo
+        return None
+
+    def eliminar_mazo(self, nombre):
+        mazo = self.obtener_mazo(nombre)
+        if mazo:
+            self._mazos.remove(mazo)
